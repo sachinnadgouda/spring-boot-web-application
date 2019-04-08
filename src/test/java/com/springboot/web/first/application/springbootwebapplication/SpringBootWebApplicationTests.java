f7fb631436360ef0cbd779d40c1acc814ea287d2
Sachin Nadgouda <sachinnadgouda@gmail.com>
2019-04-08 13:14:05 +0200

    SPRING ADD, initial commit

---
+++ src/test/java/com/springboot/web/first/application/springbootwebapplication/SpringBootWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.web.first.application.springbootwebapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
